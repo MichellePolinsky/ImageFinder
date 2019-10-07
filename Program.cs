ac61f44cd893d30dd49d0cad10b1863fd6de68fd
Michelle Polinsky <michelle.polinsky@gmail.com>
2019-10-07 17:10:42 -0400

    Stuff

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
-
+using Newtonsoft.Json;
+using GetAPhoto;
+using GetAllPhotos;
 namespace Examples.System.Net
 {
   public class WebRequestGetExample
   {
+
+
     public static void Main()
     {
       // Create a request for the URL. 		
@@ -25,16 +30,47 @@ namespace Examples.System.Net
       // Read the content.
       string responseFromServer = reader.ReadToEnd();
       // Display the content.
-      Console.WriteLine(responseFromServer);
+      // Console.WriteLine(responseFromServer);
       // Cleanup the streams and the response.
       reader.Close();
       dataStream.Close();
       response.Close();
 
       // convert the json string (responseFromServer) into a C# POCO (plain old C# Object)
+      var allPhotos = JsonConvert.DeserializeObject<GetAllPhoto[]>(responseFromServer);
       // loop over that object, foreach image 
-      // do a webrequest to get the lat/long
+      foreach (var photo in allPhotos)
+      {
+        // do a webrequest to get the lat/long
+        WebRequest single = WebRequest.Create("https://api.unsplash.com/photos/" + photo.id);
+        single.Headers.Add("Authorization", "Client-ID a43c3e64f885a5c06277e2508ad66961ba50161b989c6c8f2c97cf6634b7eff7");
+        // If required by the server, set the credentials.
+        // request.Credentials = CredentialCache.DefaultCredentials;
+        // Get the response.
+        HttpWebResponse resp = (HttpWebResponse)single.GetResponse();
+        // Display the status.
+        Console.WriteLine(resp.StatusDescription);
+        // Get the stream containing content returned by the server.
+        Stream ds = resp.GetResponseStream();
+        // Open the stream using a StreamReader for easy access.
+        StreamReader rdr = new StreamReader(ds);
+        // Read the content.
+        string respData = rdr.ReadToEnd();
+        // Display the content.
+        var thisPhoto = JsonConvert.DeserializeObject<APhoto>(respData);
+        Console.WriteLine($"{thisPhoto.id} | lat: {thisPhoto.location.position.latitude} | lng: {thisPhoto.location.position.longitude}");
+        // Cleanup the streams and the response.
+        reader.Close();
+        dataStream.Close();
+        response.Close();
+
+
+        // if (thisPhoto has a lat/lng)
+        // POST that photo to your own localhost:5001/api/CreatePhoto
+      }
+
 
     }
+
   }
 }
